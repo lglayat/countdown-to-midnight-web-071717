8e53a36dedbed6058fff7e874f00a7417c7c1cb7
Luke Glayat <glayatle@g.cofc.edu>
2017-07-07 03:10:07 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  if num == 0
+    return "HAPPY NEW YEAR!"
+  end
+
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    countdown_with_sleep(1)
+  end
+
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num)
+  sleep(num)
 end
